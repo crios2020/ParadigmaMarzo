34b0e03f3fadf0e66a1e25ab183d09abdfdee7f7
Carlos Rios <intercrios@hotmail.com>
2021-03-05 12:59:11 -0300

    Add files via upload

--- Paradigma.java
+++ Paradigma.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 public class Paradigma{
 	public static void main(String[] args){
 		//punto de entrada
@@ -8,7 +9,7 @@ public class Paradigma{
 		auto1.marca="Fiat";
 		auto1.modelo="Idea";
 		auto1.color="Rojo";
-		auto1.acelerar();			// 10
+		auto1.acelerar();			// 10			
 		auto1.acelerar();			// 20
 		auto1.acelerar();			// 30
 		auto1.frenar();				// 20
@@ -25,6 +26,36 @@ public class Paradigma{
 		}
 		System.out.println(auto2.marca+", "+auto2.modelo+", "+auto2.color+", "+auto2.velocidad);
 	
+		System.out.println("-- auto3 --");
+		Auto auto3=new Auto();
+		System.out.println(auto3.marca+", "+auto3.modelo+", "+auto3.color+", "+auto3.velocidad);
+		System.out.println(auto3.getVelocidad());
+	
+		//Las variables deben ser inicializadas
+		//int nro;
+		//String texto;
+		
+		//System.out.println(nro);			//Error
+		//System.out.println(texto);		//Error
+		
+		/*
+		 * //php
+		 * $nro;							
+		 * $texto;	
+		 * $nro='hola';	
+		 * $nro=3;
+		 * $nro=True;
+		 * $nro=3.24;
+		 * echo $nro;						//Error
+		 * echo $texto;						//Error
+		 * 
+		 * //javascript
+		 * var nro;
+		 * var texto;
+		 * alert(nro);
+		 * alert(texto);
+		 * 
+		*/ 
 		
 	} // end main
 } // end class
@@ -50,4 +81,35 @@ class Auto{
 		velocidad=velocidad-10;
 	}
 	
+	int getVelocidad(){
+		return velocidad;
+	}
+	
 } //end class
+
+class Cliente{
+	int nro;
+	String nombre;
+	int edad;
+	Cuenta cuenta;
+}
+
+class Cuenta{
+	int nro;
+	String moneda;
+	double saldo;
+}
+
+class ClientePyme{
+	
+	// Atributos aquí
+	String razonSocial;
+	String direccion;
+	Date fechaDeAlta;
+	CuentaCorriente cuenta;
+	
+	// Métodos aquí
+	
+}
+
+class CuentaCorriente {}
